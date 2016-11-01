7ab61e0a53fd716e051b2107d2c6646720927121
yvlitvin <yvlitvin@gmail.com>
2016-11-01 17:10:01 +0200

    fixed location

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ now = datetime.datetime.now()
 date = str(now.strftime("%d%m%Y"))
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 #telebot = telegram.Bot(config.token)
