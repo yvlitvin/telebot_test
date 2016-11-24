e974778a2cdd464a81b4b01871021f61a32e6ba4
yvlitvin <yvlitvin@gmail.com>
2016-11-24 09:17:09 +0200

    poll

--- poll.sh
+++ poll.sh
@@ -8,10 +8,10 @@ function vote {
      #Получаем уникальный токен
      token=`curl -s -D "$FILE_COOKIE"-d "poll_key=$POLL_KEY"http://poll.ru/index.php | tail -c 34 |cut -c -32`
      #Голосуем
-     curl -s -b "$FILE_COOKIE" -d"votes[]=$VOTE&poll_id=$POLL_ID&token=$token"http://poll.ru/vote.php &> /dev/null
+     curl -s -b "$FILE_COOKIE" -d"votes[]=$VOTE&poll_id=$POLL_ID&token=$token"http://poll.ru/vote.php
 }
 
-x=1000 #сколько голосов накрутить
+x=100 #сколько голосов накрутить
 i=1
 while [ $i -le $x ]; do
      vote
